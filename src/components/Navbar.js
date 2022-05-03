05246dc4bf20bbebd15fa0432e775b33cd28083f
arizworld <60315087+arizworld@users.noreply.github.com>
2022-05-03 20:56:40 +0530

    my second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,9 @@ export default function Navbar(props) {
                 <label className="form-check-label" htmlFor="flexSwitchCheckChecked">{capitalize(props.mode)}</label>
               </div>
             </form>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
           <div className="collapse navbar-collapse" id="navbarColor02">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
